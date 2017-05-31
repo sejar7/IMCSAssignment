1752cfedf025ab010e1697f672871c82d2bc4f8b
sejar7 <rajesh.ucmo@gmail.com>
2017-05-31 06:44:56 -0500

    First Submission

---
+++ ConsoleApp1/ConsoleApp1/Applicant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+    class Applicant
+    {
+        string firstname;
+        string lastname;
+        string phonenumber;
+        public Applicant(string firstname, string lastname, string phonenumber)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.phonenumber = phonenumber;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Object> applicantslist = new List<Object>();
+            Console.WriteLine("Enter First name");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter Last name");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number");
+            string phonenumber = Console.ReadLine();
+            Applicant client1 = new Applicant(firstname, lastname, phonenumber);
+            applicantslist.Add(client1);
+
+            Console.WriteLine("List of Applicants");
+            foreach(Object applicant in applicantslist)
+            {
+                Console.WriteLine(applicant);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
